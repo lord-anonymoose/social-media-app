f6ae8cd427101c4cc1d17eea715ed58aa6d37916
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-05-31 15:51:33 +0300

    Profile page is updated for HW3

---
+++ Social Media/ViewControllers/Profile/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  iOS_UI_HW3
+//
+//  Created by Philipp Lazarev on 25.05.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private lazy var profileView: ProfileHeaderView = {
+        let profileView = ProfileHeaderView()
+        profileView.translatesAutoresizingMaskIntoConstraints = false
+        return profileView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        view.addSubview(profileView)
+        NSLayoutConstraint.activate([
+            profileView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            profileView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            profileView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            profileView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+        ])
+    }
+}
