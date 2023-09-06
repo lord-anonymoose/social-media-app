89409af3dc1ba2a076fcd691609c457927d28f45
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-09-06 15:47:31 +0300

    PhotosViewController MVP

---
+++ Social Media/Modules/Profile/ProfilePhotos/PhotosCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Social Media
+//
+//  Created by Philipp Lazarev on 06.09.2023.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    private enum constants {
+        static let verticalSpacing: CGFloat = 8.0
+        static let horizontalSpacing: CGFloat = 8.0
+        static let topPadding: CGFloat = 8.0
+    }
+    
+    // MARK: - Subviews
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupView()
+        addSubviews()
+        setupConstraints()
+    }
+    
+    // MARK: - Private
+    
+    private func setupView() {
+        contentView.clipsToBounds = true
+        contentView.backgroundColor = UIColor(named: "BackgroundColor")
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(imageView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Public
+    
+    func setup(with photo: UIImage) {
+        imageView.image = photo
+    }
+}
--- Social Media/Modules/Profile/ProfilePhotos/PhotosViewController.swift
+++ Social Media/Modules/Profile/ProfilePhotos/PhotosViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UICollectionViewDelegate {
+    
+    // MARK: - Data
     
     // MARK: - Subviews
-    
-    private lazy var photoCollectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+    private let photoCollectionView: UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        
+        collectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
+        
         return collectionView
     }()
     
@@ -32,16 +41,52 @@ class PhotosViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = UIColor(named: "BackgroundColor")
+        title = "Photo gallery"
     }
     
     private func addSubviews() {
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
         view.addSubview(photoCollectionView)
+        
+        photoCollectionView.dataSource = self
+        photoCollectionView.delegate = self
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            
+            photoCollectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            photoCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            photoCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            photoCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
     }
-    
 }
+
+extension PhotosViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        myPhotos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotosCollectionViewCell
+        
+        let photo = myPhotos[indexPath.row]
+        cell.setup(with: photo)
+        
+        return cell
+    }
+}
+
+extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let countItem: CGFloat = 3
+        let accessibleWidth = collectionView.frame.width - 32
+        let widthItem = (accessibleWidth / countItem)
+        return CGSize(width: widthItem, height: widthItem * 0.56)
+    }
+}
+
