a4914509560829fe29659894e218cd1803ca05f5
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-09-08 22:21:49 +0300

    Moved User and Post structs to StorageService Framework

--- Social Media/Constant.swift
+++ Social Media/Constant.swift
@@ -8,33 +8,5 @@
 import Foundation
 import UIKit
 
-public struct User {
-    let id = UUID()
-    var login: String
-    var name: String
-    
-    init(login: String, name: String) {
-        self.login = login
-        self.name = name
-    }
-}
-
 let accentColor = UIColor(hex: "#01937C")
 let secondaryColor = UIColor(hex: "#FFC074")
-
-public struct Post {
-    let id = UUID()
-    let author: String
-    let description: String
-    let image: String
-    let likes: Int
-    let views: Int
-    
-    init(author: String, description: String, image: String, likes: Int, views: Int) {
-        self.author = author
-        self.description = description
-        self.image = image
-        self.likes = likes
-        self.views = views
-    }
-}
