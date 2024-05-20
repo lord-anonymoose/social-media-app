9a84fae9397ab1b86da93128f51597909a6f612a
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-05-21 01:05:31 +0300

    Added Service for fetching user posts

---
+++ Social Media/Modules/Feed/FeedViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewModel.swift
+//  Social Media
+//
+//  Created by Philipp Lazarev on 21.05.2024.
+//
+
+import Foundation
+import UIKit
+
+/*
+final class FeedViewModel: PostsVMOutput {
+    var state: State
+    
+    var currentState: ((State) -> Void)?
+    
+    func changeState() {
+        <#code#>
+    }
+    
+    
+    private let service: Service
+}
+*/
